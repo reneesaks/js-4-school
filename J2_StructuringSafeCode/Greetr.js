8a85236e65f72c97abb9986ac28eba9ff73ecf34
reneesaks <renee.saks@khk.ee>
2017-01-22 23:26:31 +0200

    Structuring safe code

---
+++ J2_StructuringSafeCode/Greetr.js
@@ -0,0 +1,5 @@
+(function(global, $) { // immediately invoked function passed with global object and jquery
+    
+    // safe code
+    
+}(window, jQuery)); // invoked it and passed jquery object and global object
